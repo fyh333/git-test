055424c52a0394cf09ea740f67bf79a817705234
fangyuheng <fangyuheng@cmiot.chinamobile.com>
2026-06-24 18:34:48 +0800

    feature: rm test args

--- a.c
+++ a.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-int test(int x, int b) {
-  return x + b + 1;
+void hello() {
+  printf("hello world\n");
 }
 
 int main(int argc, char *argv[]) {
-  int a = test(1, 2);
-
-  printf("a: %d\n", a);
+  hello();
 
   return 0;
 }
