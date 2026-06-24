0b57d9b1ac21a140bb94f7c4661cb52f3dc6d6b1
fangyuheng <fangyuheng@cmiot.chinamobile.com>
2026-06-24 18:21:55 +0800

    add test function

--- a.c
+++ a.c
@@ -1,3 +1,13 @@
-111aaa
-qqq
-zhongguofei
+#include <stdio.h>
+
+int test(int a) {
+  return a;
+}
+
+int main(int argc, char *argv[]) {
+  int a = test(1);
+
+  printf("a: %d\n", a);
+
+  return 0;
+}
